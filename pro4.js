b9d2db40a4600f34472cb63e5dc30a0927b53b06
Jonas Rocha <97409941+Jonas1611@users.noreply.github.com>
2022-02-14 19:57:26 -0300

    Add files via upload

--- pro4.js
+++ pro4.js
@@ -1,13 +1,24 @@
 const prompt = require("prompt-sync")();
 
-const humor = [
-  "Triste",
-  "Feliz",
-  "Frustrado",
-  "Normal",
-  "Estressado",
-  "Euforico",
-];
+const bruxo = {
+  nome: "Crowster",
+  armadura: ["sim"],
+  humor: ["mal"],
+  vida: 100,
+  atençao: 100,
+  energia: 100,
+  vigor: 100,
+  fome: 5,
+  sede: 5,
+  nivel: "Salvador",
+  itens: " ",
+};
+
+const pessoasBar = {
+  garcons: "",
+  bruxos: "",
+  mosntros: "",
+};
 
 const docas = () => {
   console.log(`Introduzir os codigos do cenário docas`);
@@ -22,43 +33,71 @@ const torre = () => {
 };
 
 const local = () => {
-  const cenario = prompt(`Para onde deseja ir? `).toLowerCase();
+  console.log(`Para qual local deseja ir?`);
+
+  const cenario = prompt(`digite sua escolha: `).toLowerCase();
 
   if (cenario === "docas") {
-    
     return docas();
-
   } else {
-    if(cenario === "centro da vila") {
-
+    if (cenario === "centro da vila") {
       return centro();
-
-    } else if (cenario === "torre de vigia"){
-
+    } else if (cenario === "torre de vigia") {
       return torre();
-    } else if (cenario === "taberna"){
-
+    } else if (cenario === "taberna") {
       return taberna();
-
     } else {
-      console.log(`Local inexistente tente novamente`)
+      console.log(`Local inexistente tente novamente`);
 
       return local();
     }
-
   }
-}
+};
 
-const taberna = () => {
-  console.log(`Bem-vindo a Taberna grande Bruxo!`);
+const conversar = () => {
+  console.log(`Pessoas na taberna`);
+  console.log(pessoasBar);
 
+  let convera = prompt(`Com quem deseja conversar? `);
 
-  for (; ;) {
-    let resphumor = prompt(`Como está se sentindo agora? `);
+  if (conversa === "garcons") {
+    console.log(`A conversa alterou o humor do bruxo`);
 
-    if (resphumor !== "feliz") {
-      let resphumor = prompt(`Como deseja melhora? `).toLowerCase();
+    bruxo.humor = "Bom";
+
+  } else if (conversa === "bruxos") {
+    console.log(`A conversa alterou o humor do bruxo`);
+
+    bruxo.humor = "Ótimo";
+
+  } else if (convera === "monstros"); {
     
+    console.log(`A conversa alterou o humor do bruxo`);
+
+    bruxo.humor = "Entusiasmado";
+  }
+};
+
+const bebida = () => {
+  let bebidas = prompt(`O que deseja beber? `);
+};
+
+const garcom = () => {
+  for (;;) {
+    console.log(bruxo.humor[0]);
+
+    if (bruxo.humor[0] !== "feliz") {
+      let resp = prompt(`O Que deseja? `).toLowerCase();
+
+      if (resp === "conversar") {
+        conversar();
+      } else if (resp === "bebida") {
+        bebida();
+      } else {
+        console.log(
+          `Não temos está opção, escolha outra coisa que deseja fazer`
+        );
+      }
     } else {
       const irVila = prompt("Vamos reconstruir a vila? ").toLowerCase();
 
@@ -68,6 +107,12 @@ const taberna = () => {
     }
     break;
   }
+};
+
+const taberna = () => {
+  console.log(`Bem-vindo a Taberna grande Bruxo!`);
+
+  garcom();
 };
 
 taberna();
